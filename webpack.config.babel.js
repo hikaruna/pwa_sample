af54bc5ee0cf5b70808a8d636cc26a057ea4313b
hikaruna <hikaru@hikaruna.net>
2018-06-08 21:56:49 +0900

    wip やりかけ2 webpack config中

---
+++ webpack.config.babel.js
@@ -0,0 +1,22 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  reles: {
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+}
